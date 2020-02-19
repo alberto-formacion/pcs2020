5433751e5afb2bba545ea46d8713a753fb494a3b
Alberto Garcia de Cortazar Rouret <mum@10.11.5.124>
2020-02-19 19:28:17 +0100

    filtro

---
+++ proyectoPCSclase/src/com/centrosanluis/proyectoPCSclase/filter/FiltroLogin.java
@@ -0,0 +1,31 @@
+package com.centrosanluis.proyectoPCSclase.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/privado/*")
+public class FiltroLogin implements Filter {
+
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		if(req.getAttribute("usuario")!=null) {
+			chain.doFilter(request, response);
+		}else {
+			res.sendRedirect("../login.jsp");;
+		}
+		
+		
+	}
+
+}
