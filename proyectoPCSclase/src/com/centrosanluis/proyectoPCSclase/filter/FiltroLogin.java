887c83abaa70f3737f85a48542e94072750bb5f7
Alberto Garcia de Cortazar Rouret <mum@172.30.22.44>
2020-03-06 20:22:35 +0100

    filtro

--- proyectoPCSclase/src/com/centrosanluis/proyectoPCSclase/filter/FiltroLogin.java
+++ proyectoPCSclase/src/com/centrosanluis/proyectoPCSclase/filter/FiltroLogin.java
@@ -28,16 +28,4 @@ public class FiltroLogin implements Filter {
 		
 	}
 
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
