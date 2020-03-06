bada99dbeeb9acb85691715d8d643903c9586961
Alumno <Alumno@CSL-P2-A22-50>
2020-03-06 20:18:35 +0100

    cambio filtro

--- proyectoPCSclase/src/com/centrosanluis/proyectoPCSclase/filter/FiltroLogin.java
+++ proyectoPCSclase/src/com/centrosanluis/proyectoPCSclase/filter/FiltroLogin.java
@@ -28,4 +28,16 @@ public class FiltroLogin implements Filter {
 		
 	}
 
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
